ddc5b0a1c0ddad8f61270ebbf3da679f9e24df7e
ahmetemrekilinc <aekilinc@gmail.com>
2022-04-05 10:16:10 +0300

    3.2.0

--- storybook/src/App.js
+++ storybook/src/App.js
@@ -1,18 +1,8 @@
-import { GTMProvider } from '@elgorditosalsero/react-gtm-hook';
 import './App.css';
 import Main from './components/Main';
 
 function App() {
-    const gtmParams = { id: process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID };
-    let appJsx = <Main />;
-    if (process.env.NODE_ENV === 'production') {
-        appJsx = (
-            <GTMProvider state={gtmParams}>
-                <Main />
-            </GTMProvider>
-        );
-    }
-    return appJsx;
+    return <Main />;
 }
 
 export default App;
